9818aab7e419c1895f57c6495deebc991c2e480b
GrantRoots <158632641+GrantRoots@users.noreply.github.com>
2025-03-25 17:01:44 -0700

    fix: make app.js load each page by url

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 http
   .createServer((req, res) => {
-    fs.readFile("index.html", (err, data) => {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-    });
+    const getUrl = url.parse(req.url, true);
+    const filename = "." + getUrl.pathname + ".html";
+    if (getUrl.pathname == "/") {
+      fs.readFile("index.html", (err, data) => {
+        if (err) {
+          res.writeHead(404, { "Content-Type": "text/html" });
+          return console.error(err);
+        }
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    } else {
+      fs.readFile(filename, (err, data) => {
+        if (err) {
+          res.writeHead(404, { "Content-Type": "text/html" });
+          return console.error(err);
+        }
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    }
   })
   .listen(8080);
